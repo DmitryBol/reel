faaa11043bca4014752ba7cc143c0899f36fe4b8
Dmitry <bolokhovdn@gmail.com>
2018-05-05 22:21:05 +0300

    сгенерированные барабаны с разными частотами подсчет числа убитых хороших символов при expand подсчет числе first с числами second и third проблемы

--- reel generator.py
+++ reel generator.py
@@ -53,49 +53,110 @@ def g(bag, available, length, reel, alpha):
 
 class reel_generator(object):
     def __init__(self, array, names):
-        tmp = []
+        self.reels = []
+        for k in range(len(array)):
+            tmp = []
+            for i in range(len(array[k])):
+                for j in range(array[k][i]):
+                    tmp.append('empty')
+            self.reels.append(tmp)
+        #self.len = len(tmp)
+
         for i in range(len(array)):
-            for j in range(array[i]):
-                tmp.append('empty')
-        reel_1 = copy.deepcopy(tmp)
-        reel_2 = tmp.copy()
-        reel_3 = tmp.copy()
-        reel_4 = tmp.copy()
-        reel_5 = tmp.copy()
-        self.len = len(tmp)
-        self.reels = [reel_1, reel_2, reel_3, reel_4, reel_5]
-        for i in range(5):
             alpha = 1
-            bag = copy.deepcopy(frequency)
-            available = np.arange(len(array))
-            tmp = g(bag, available, self.len, self.reels[i], alpha)
+            bag = copy.deepcopy(array[i])
+            available = np.arange(len(array[i]))
+            tmp = g(bag, available, len(self.reels[i]), self.reels[i], alpha)
             vse_horosho = tmp[0]
             while(not vse_horosho):
-                bag = copy.deepcopy(frequency)
+                bag = copy.deepcopy(array[i])
                 alpha += h
                 available = np.arange(len(array))
                 np.delete(available, [names.index(tmp[1][len(tmp[1]) - 1]),names.index(tmp[1][len(tmp[1]) - 2])])
-                tmp = g(bag, available, self.len, self.reels[i], alpha)
+                tmp = g(bag, available, len(self.reels[i]), self.reels[i], alpha)
                 vse_horosho = tmp[0]
             self.reels[i] = tmp[1]
 
 
+
+
+
 #конструктор принимает на вход два массива: 1ый - массив количества элементов, 2ой- их имена. Класс содержит список барабанов
 def print_game(test):
-    for i in range(len(test.reels[0])):
-        print(test.reels[0][i], end=(9 - len(test.reels[0][i]))*' ')
-        print(test.reels[1][i], end=(9 - len(test.reels[1][i]))*' ')
-        print(test.reels[2][i], end=(9 - len(test.reels[2][i]))*' ')
-        print(test.reels[3][i], end=(9 - len(test.reels[3][i]))*' ')
-        print(test.reels[4][i], end=(9 - len(test.reels[4][i]))*' ')
+    c = []
+    for i in range(len(test.reels)):
+        c.append(len(test.reels[i]))
+    a = max(c)
+    for i in range(a):
+        for j in range(len(test.reels)):
+            if(i < len(test.reels[j])):
+                print(test.reels[j][i], end=(9 - len(test.reels[j][i]))*' ')
+            else:
+                s = 8*' '
+                print(s, end=' ')
         print('\n')
 #распечатывает сгенерированные барабаны
 
-def is_wild(a):
+def is_expand_wild(a):
     if(str(a) == 'Crown'):
         return True
     else:
         return False
+
+def count_killed(reel, game, line, element):
+    m = 0
+    for i in range(len(game.reels[reel])):
+        if(line[reel] == 1):
+            if(game.reels[reel][i] == element):#здесь еще нужна проверка на wild - or is_wild(game.reels[reel][i])
+                if(is_expand_wild(game.reels[reel][(i + 1) % len(game.reels[reel])]) or is_expand_wild(game.reels[reel][(i + 2) % len(game.reels[reel])])):
+                    m += 1
+        elif(line[reel] == 2):
+            if(game.reels[reel][i] == element):
+                if(is_expand_wild(game.reels[reel][(i - 1) % len(game.reels[reel])]) or is_expand_wild(game.reels[reel][(i + 1) % len(game.reels[reel])])):
+                    m += 1
+    if(line[reel] == 3):
+        if(game.reels[reel][i] == element):
+            if(is_expand_wild(game.reels[reel][(i - 1) % len(game.reels[reel])]) or is_expand_wild(game.reels[reel][(i - 2) % len(game.reels[reel])])):
+                m += 1
+    return(m)
+
+def count_combinations(game, line, element, length, names):
+    k = []
+    w = []
+    e = []
+    m = []
+    n = []
+    for i in range(5):
+        reel = i
+        ind = names.index(element)
+        k.append(frequency[i][ind])
+        w.append(0)#в shining crown нет обычных wild
+        n.append(len(game.reels[i]))
+        tmp = 0
+        for j in range(len(names)):
+            if(is_expand_wild(names[j])):
+                tmp += frequency[i][j]
+        e.append(tmp)
+        m.append(count_killed(reel, game, line, element))
+    for i in range(2):
+        k.append(0)
+        w.append(0)
+        e.append(0)
+        m.append(0)
+        n.append(1)
+
+    tmp = 1
+    for i in range(length):
+        tmp = tmp*(k[i] + w[i] + 3*e[i] - m[i])
+    tmp = tmp*(n[length] - k[length] - w[length] - 3*e[length] + m[length])
+    for i in range(length+1, 7):
+        tmp = tmp*n[i]
+    print(k, w, e, m, n)
+    print(line)
+    first = tmp
+    print(first)
+    return(first)
+
 
 
 
@@ -104,8 +165,12 @@ text = f.read()
 rules = json.loads(text)
 
 
-frequency = [3, 7, 3, 3, 2, 3, 4, 15, 3, 6, 3]
-
+frequency_1 = [3, 5, 3, 3, 2, 3, 4, 2, 0, 1, 3]
+frequency_2 = [3, 5, 3, 3, 2, 3, 4, 2, 2, 1, 0]
+frequency_3 = [3, 5, 3, 3, 2, 3, 4, 2, 3, 1, 3]
+frequency_4 = [3, 5, 3, 3, 2, 3, 4, 2, 4, 1, 0]
+frequency_5 = [3, 5, 3, 3, 2, 3, 4, 2, 0, 1, 3]
+frequency = [frequency_1, frequency_2, frequency_3, frequency_4, frequency_5]
 
 names = []
 for i in range(len(rules['symbol'])):
@@ -114,15 +179,12 @@ for i in range(len(rules['symbol'])):
 
 game = reel_generator(frequency, names)
 
+
+
 print_game(game)
 
+count_combinations(game, rules['lines'][3], 'Melon', 4, names)
 
 
 
 
-
-#print_game(game)
-
-#counter = counting_combinations(rules['symbol'][0]['name'], 7, 3, rules)
-
-#print('counter is ', counter)
