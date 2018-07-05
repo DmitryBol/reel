292b5363f942dd58a5a36147bc03a1c0c72cc85b
milk1 <amvasylev@gmail.com>
2018-07-05 22:39:27 +0300

    * simulate.py теперь проводит в том числе фригеймы, считает RTP и SD

--- simulate.py
+++ simulate.py
@@ -9,14 +9,65 @@ import numpy as np
 
 # noinspection SpellCheckingInspection,PyShadowingNames
 def scatter_payment(obj, gametype, matrix):
-    cnt = 0
     res = 0
     for scat in gametype.scatterlist:
+        cnt = 0
         for i in range(obj.window[1]):
             for j in range(obj.window[0]):
                 if matrix[i, j] == scat:
                     cnt += 1
         res += gametype.symbol[scat].payment[cnt] * len(obj.line)
+
+        if gametype.symbol[scat].scatter[cnt] > 0:
+            for _ in range(gametype.symbol[scat].scatter[cnt]):
+                res += make_spin('free')
+    return res
+
+
+def make_spin(type):
+    res = 0
+    if type == 'base':
+        for i in range(obj.window[0]):
+            temp = random.randint(0, len(obj.base.reels[i]))
+            for j in range(obj.window[1]):
+                matrix[obj.window[1] - 1 - j, i] = obj.base.symbol.index(
+                    obj.base.reels[i][(temp - j) % len(obj.base.reels[i])])
+
+        for i in range(obj.window[1]):
+            for j in range(obj.window[0]):
+                if matrix[i, j] in obj.base.ewildlist:
+                    for k in range(obj.window[1]):
+                        matrix[k, j] = matrix[i, j]
+
+        res += scatter_payment(obj, obj.base, matrix)
+        for line in obj.line:
+            string = np.array([matrix[line[i] - 1, i] for i in range(obj.window[0])])
+            width = obj.window[0]
+            line_combination = obj.base.get_combination(string, width)
+            for comb in line_combination:
+                res += obj.base.symbol[comb[0]].payment[comb[1]]
+
+    if type == 'free':
+        for i in range(obj.window[0]):
+            temp = random.randint(0, len(obj.free.reels[i]))
+            for j in range(obj.window[1]):
+                matrix[obj.window[1] - 1 - j, i] = obj.free.symbol.index(
+                    obj.free.reels[i][(temp - j) % len(obj.free.reels[i])])
+
+        for i in range(obj.window[1]):
+            for j in range(obj.window[0]):
+                if matrix[i, j] in obj.free.ewildlist:
+                    for k in range(obj.window[1]):
+                        matrix[k, j] = matrix[i, j]
+
+        res += scatter_payment(obj, obj.free, matrix)
+        for line in obj.line:
+            string = np.array([matrix[line[i] - 1, i] for i in range(obj.window[0])])
+            width = obj.window[0]
+            line_combination = obj.free.get_combination(string, width)
+            for comb in line_combination:
+                res += obj.free.symbol[comb[0]].payment[comb[1]] * obj.free_multiplier
+
     return res
 
 
@@ -35,72 +86,28 @@ frequency_5 = [5, 6, 6, 6, 6, 6, 14, 16, 16, 16, 16, 16, 4]
 
 frequency = [frequency_1, frequency_2, frequency_3, frequency_4, frequency_5]
 
-REPEAT_CNT = 100000000
+REPEAT_CNT = 1000000
 
 matrix = np.zeros((obj.window[1], obj.window[0]))
 
-total_payment = 0
+obj.base.reel_generator(frequency, obj.window[0], obj.distance)
+obj.free.reel_generator(frequency, obj.window[0], obj.distance)
+obj.base.fill_frequency(frequency)
+obj.free.fill_frequency(frequency)
 
-WILD = 0
-
-bad = True
-counter = 0
-while bad and counter < 100000:
-
-    obj.base.reel_generator(frequency, obj.window[0])
-    obj.free.reel_generator(frequency, obj.window[0])
-    obj.base.fill_frequency(frequency)
-    obj.free.fill_frequency(frequency)
-
-    #print(frequency)
-
-    bad = False
-
-    for reel in obj.base.reels:
-        for symbol_index in range(len(reel)):
-            symbol = reel[symbol_index]
-            near = []
-            for i in range(-obj.window[1] + 1, 0, 1):
-                near.append(reel[(symbol_index + i) % len(reel)].name)
-            if symbol.name == 'wild' and 'Scat' in near:
-                bad = True
-    counter += 1
-if counter == 100000:
-    exit(0)
-
+#все выплаты в тотал бетах
+payments_sum = 0
+payments_square_sum = 0
 for cnt in range(REPEAT_CNT):
-    for i in range(obj.window[0]):
-        temp = random.randint(0, len(obj.base.reels[i]))
-        for j in range(obj.window[1]):
-            matrix[obj.window[1] - 1 - j, i] = obj.base.symbol.index(obj.base.reels[i][(temp - j) % len(obj.base.reels[i])])
+    spin_result = make_spin('base')
+    payments_sum += spin_result / len(obj.line)
+    payments_square_sum += (spin_result / len(obj.line))**2
+    if (cnt + 1) % int(REPEAT_CNT/100) == 0:
+        print(str(round((cnt + 1) / REPEAT_CNT * 100)) + '%')
 
-    for i in range(obj.window[1]):
-        for j in range(obj.window[0]):
-            if matrix[i, j] == WILD:
-                for k in range(obj.window[1]):
-                    matrix[k, j] = WILD
+rtp = payments_sum / REPEAT_CNT
+sd = (1/(REPEAT_CNT - 1) * (payments_square_sum - 1/REPEAT_CNT * payments_sum**2))**0.5
+print('simulation rtp = ', rtp)
+print('simulation sd = ', sd)
 
-    if cnt % int(REPEAT_CNT/100) == 0:
-        print(matrix, '\n')
 
-    total_payment += scatter_payment(obj, obj.base, matrix)
-    for line in obj.line:
-        string = np.array([matrix[line[i] - 1, i] for i in range(obj.window[0])])
-        width = obj.window[0]
-        line_combination = obj.base.get_combination(string, width)
-        for comb in line_combination:
-            total_payment += obj.base.symbol[comb[0]].payment[comb[1]]
-
-rtp = total_payment / (len(obj.line) * REPEAT_CNT)
-print('rtp_simulate = ', rtp)
-
-obj.base.fill_num_comb(obj.window, obj.line)
-obj.free.fill_num_comb(obj.window, obj.line)
-print('All combinations =', obj.base.all_combinations())
-LAL = obj.freemean()
-print('Freemean =', LAL)
-print('Base RTP =', obj.count_baseRTP())
-rtp = obj.count_RTP(LAL)
-print('RTP =', rtp)
-print('Volatility =', obj.count_volatility(LAL, rtp))
-print('Hitrate =', obj.count_hitrate())
